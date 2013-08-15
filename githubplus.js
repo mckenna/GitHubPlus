ed7c9a64c1531d4137e8a613259d0bcf7f52c665
Peter McKenna <pmckenna@gmail.com>
2013-08-15 10:53:45 +0100

    Remember selection of PRs v Issues. Remove selector from PRs page

--- githubplus.js
+++ githubplus.js
@@ -1,13 +1,14 @@
 $(function() {
-  add_extra_issue_list_controls();
-
   var add_extra_issue_list_controls = function() {
     var issueCheck;
-    if($('.issues-list-options').length > 0) {
+    if($('#issues_list .issues-list-options').length > 0) {
       issueCheck = window.setInterval(
         function() {
           if( $('.dynamically-inserted').length === 0){
             $('.issues-list-options').append(button_template);
+            chrome.storage.sync.get('githubplus.issuesShow', function(items) {
+              $('.issues-list-options').find('.dynamically-inserted [data-show="'+items['githubplus.issuesShow']+'"]').click()
+            });
           }
         },
         500
@@ -18,6 +19,10 @@ $(function() {
     }
   }
 
+  // Remove whitespace from GitDiffs automatically
+  $('[data-container-id="files_bucket"]').attr('href', function(i,href) { return href + '?w=0'; });
+
+  // Make the auto inserted buttons work
   $(document).on(
     'click',
     '.dynamically-inserted a',
@@ -35,17 +40,21 @@ $(function() {
       if($show === 'issues') {
         $issues.show();
         $prs.hide();
+        chrome.storage.sync.set({'githubplus.issuesShow': 'issues'});
       }
       else if($show === 'prs') {
         $issues.hide();
         $prs.show();
+        chrome.storage.sync.set({'githubplus.issuesShow': 'prs'});
       }
       else {
         $issues.show();
         $prs.show();
+        chrome.storage.sync.set({'githubplus.issuesShow': 'all'});
       }
     }
   );
 
   var button_template = '<div class="button-group dynamically-inserted"><a href="#" class="minibutton selected" data-show="all">All</a><a href="#" class="minibutton" data-show="issues"><span class="type-icon octicon octicon-issue-opened"></span></a><a href="#" class="minibutton" data-show="prs"><span class="type-icon octicon octicon-git-pull-request"></span></a></div>';
+  add_extra_issue_list_controls();
 });
